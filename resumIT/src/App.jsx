093cecc39748edec36fe97a46f874943b3ca36c1
asif7649 <asifahma7761@gmail.com>
2025-04-12 23:00:36 +0530

    resume mecker app

---
+++ resumIT/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className=" bg-red-500 text-white text-2xl">
+        ResumIT
+      </div>
+      
+    </>
+  )
+}
+
+export default App
